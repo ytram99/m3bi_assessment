044863cee31be22597150d01bd8293cadde7d1ed
thulasiramyachamaneni <ram@trajectory-solutions.com>
2019-06-23 15:55:51 -0700

    Junit

---
+++ src/test/java/com/assignment/weatherforecast/controller/WeatherForecastControllerTest.java
@@ -0,0 +1,8 @@
+package com.assignment.weatherforecast.controller;
+
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class WeatherForecastControllerTest {
+}
